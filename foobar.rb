37643fd0b731919e7f0a3aa41e81cdd82e3bf946
JacoboSternberg <jacobo21blue@gmail.com>
2015-09-17 21:03:47 -0700

    ALL TESTS PASSING

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
   # Q4 CODE HERE
+
+  def self.baz(a)
+    a = a.collect{|x| x.to_i}
+    a = a.uniq
+    a = a.select{|x| x%2 == 0}
+    a = a.select{|x| x < 9}
+    a = a.collect{|x| x + 2}
+    sum = a.reduce :+
+  end
+
 end
